62489682106e4710661f77ca9c1c67bb3bd2daf0
ngotiendat <datmau20@gmail.com>
2022-09-21 21:03:02 +0700

    add tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular';
+  // title = '<h1 class="bg-red-600">angular</h1>';
 }
