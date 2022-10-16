01c2b49cf2853ccdabd5f24a640b418c3e3eeba0
ngotiendat <datmau20@gmail.com>
2022-10-16 11:23:01 +0700

    create-update-remove/product-user

---
+++ src/app/service/config.service.ts
@@ -0,0 +1,1 @@
+export const apiPort = 4000;
--- src/app/service/product/product-service.service.ts
+++ src/app/service/product/product-service.service.ts
@@ -2,15 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IProduct } from 'src/app/interface/IProducts';
+import { apiPort } from '../config.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  API = 'http://localhost:4000/products';
+  API = `http://localhost:${apiPort}/products`;
   constructor(private http: HttpClient) {}
 
   getAllProduct(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.API);
+  }
+  getProduct(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.API}/${id}`);
+  }
+  createProduct(data: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(this.API, data);
+  }
+  updateProduct(data: IProduct): Observable<IProduct> {
+    return this.http.put<IProduct>(`${this.API}/${data.id}`, data);
+  }
+  removeProduct(id: number): Observable<IProduct> {
+    return this.http.delete<IProduct>(`${this.API}/${id}`);
   }
 }
