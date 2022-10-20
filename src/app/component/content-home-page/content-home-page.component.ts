7ad87e3fe075fd5dca41de0d9ec87d174a8394be
ngotiendat <datmau20@gmail.com>
2022-10-20 09:15:59 +0700

    update component

---
+++ src/app/component/content-home-page/content-home-page.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/interface/IProducts';
+import { CategoryService } from 'src/app/service/categories/category.service';
+import { ProductService } from 'src/app/service/product/product-service.service';
+
+@Component({
+  selector: 'app-content-home-page',
+  templateUrl: './content-home-page.component.html',
+  styleUrls: ['./content-home-page.component.scss'],
+})
+export class ContentHomePageComponent implements OnInit {
+  products: IProduct[] = [];
+
+  filteredProducts: IProduct[] = [];
+  categories: any = [];
+  message: string = '';
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProduct();
+
+    this.categoryService.getAllCategory().subscribe(
+      (cate) => {
+        this.categories = cate;
+      },
+      (error) => {
+        this.message = error.message;
+      }
+    );
+  }
+
+  getProduct() {
+    this.productService.getAllProduct().subscribe(
+      (data) => {
+        this.products = data;
+        this.filteredProducts = this.products;
+        console.log(this.products);
+      },
+      (error) => {
+        this.message = error.message;
+      }
+    );
+  }
+
+  handleClickCategories(id: any) {
+    switch (id) {
+      case this.categories[0].id:
+        this.filteredProducts = this.products.filter((item: any) => {
+          return item.id_categories === id;
+        });
+        break;
+
+      case this.categories[1].id:
+        this.filteredProducts = this.products.filter((item: any) => {
+          return item.id_categories === id;
+        });
+        break;
+
+      case this.categories[2].id:
+        this.filteredProducts = this.products.filter((item: any) => {
+          return item.id_categories === id;
+        });
+        break;
+
+      case this.categories[3].id:
+        this.filteredProducts = this.products.filter((item: any) => {
+          return item.id_categories === id;
+        });
+        break;
+      default:
+        this.filteredProducts = this.products;
+        break;
+    }
+  }
+}
