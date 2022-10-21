a4c28104a96a6ef0f599dec7a5b4c10f0c288d1d
ngotiendat <datmau20@gmail.com>
2022-10-21 09:22:09 +0700

    genaral

--- src/app/page/general/general.component.ts
+++ src/app/page/general/general.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IUser } from 'src/app/interface/IUser';
+import { UserService } from 'src/app/service/user/user.service';
 
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.scss']
+  styleUrls: ['./general.component.scss'],
 })
 export class GeneralComponent implements OnInit {
+  user!: IUser;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.getUser();
   }
 
+  getUser() {
+    this.activatedRoute.params.subscribe((params) => {
+      const idParams = params['id'];
+
+      this.userService.getUser(idParams).subscribe((user) => {
+        this.user = user;
+      });
+    });
+  }
 }
--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -1,3 +1,3 @@
 // export const apiPort = 4000;
-// export const API = 'https://angular-api-two.vercel.app/api';
-export const API = 'http://localhost:4000/api';
+export const API = 'https://angular-api-two.vercel.app/api';
+// export const API = 'http://localhost:4000';
